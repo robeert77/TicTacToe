21585582c068ffb166e80dc5b1e41421f29e237c
robeert77 <r.ionatan77@gmail.com>
2021-05-10 22:38:20 +0300

    changed checkGameStatus function

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function replay() {
 
 function generateGrid() {
     for (let i = 0; i < 9; i++)
-        $('#grid').append('<button type="button" class="btn gridCell border border-dark rounded-0 fs-4" id="cell'
-        + i + '" onclick="markCell(' + i + ');"></button>');
+        $('#grid').append('<button type="button" class="btn gridCell border border-dark rounded-0 fs-4" id="cell' + i + '" onclick="markCell(' + i + ');"></button>');
 }
 
 function setNames() {
@@ -35,34 +34,30 @@ function markCell(whichCell) {
     else
         $('#cell' + whichCell).text('0');
     whoTurn = 3 - whoTurn;
-    $('#messagesForPlayers').text(names[whoTurn] + '\'s turn.');
     nrMarkedCells++;
-    checkGameStatus();
+    $('#messagesForPlayers').text(names[whoTurn] + '\'s turn.');
+    checkGameStatus( whichCell - whichCell % 3, whichCell % 3);
 }
 
-function checkGameStatus() {
-    if (nrMarkedCells === 9) {
+function checkGameStatus(line, column) {
+    var whoWon = 0;
+    if ($('#cell' + line).text() === $('#cell' + (line + 1)).text() && $('#cell' + line).text() === $('#cell' + (line + 2)).text()) // check for line winning
+        whoWon = 2 - ($('#cell' + line).text() === 'X');
+    if ($('#cell' + column).text() === $('#cell' + (column + 3)).text() && $('#cell' + column).text() === $('#cell' + (column + 6)).text()) // check for column winning
+        whoWon = 2 - ($('#cell' + column).text() === 'X');
+    if ($('#cell0').text() !== '' && $('#cell0').text() === $('#cell4').text() && $('#cell0').text() === $('#cell8').text()) // check for main diagonal winning
+        whoWon = 2 - ($('#cell0').text() === 'X');
+    if ($('#cell2').text() !== '' && $('#cell2').text() === $('#cell4').text() && $('#cell2').text() === $('#cell6').text()) // check for secondary diagonal winning
+        whoWon = 2 - ($('#cell2').text() === 'X');
+
+    if (whoWon) {
+        $('#messagesForPlayers').text(names[whoWon] + ' has won!');
+        $('#informationSpace').append('<button type="button" class="btn btn-info rounded-pill px-3" onclick="replay();">Play Again!</button>');
+        invalidCells();
+    }
+    else if (nrMarkedCells === 9) {
         $('#messagesForPlayers').text('Draw. Try again!');
         $('#informationSpace').append('<button type="button" class="btn btn-info rounded-pill px-3" onclick="replay();">Play Again!</button>');
-    }
-    else {
-        var whoWon = 0;
-        for (let i = 0; i < 7; i += 3) {
-            if ($('#cell' + i).text() === $('#cell' + (i + 1)).text() && $('#cell' + i).text() === $('#cell' + (i + 2)).text() && $('#cell' + i).text() !== '')
-                whoWon = 2 - ($('#cell' + i).text() === 'X');
-            if ($('#cell' + i / 3).text() === $('#cell' + (i / 3 + 3)).text() && $('#cell' + i / 3).text() === $('#cell' + (i / 3 + 6)).text() && $('#cell' + i / 3).text() !== '')
-                whoWon = 2 - ($('#cell' + i / 3).text() === 'X');
-        }
-        if ($('#cell0').text() === $('#cell4').text() && $('#cell0').text() === $('#cell8').text() && $('#cell0').text() !== '')
-            whoWon = 2 - ($('#cell0').text() === 'X');
-        if ($('#cell2').text() === $('#cell4').text() && $('#cell2').text() === $('#cell6').text() && $('#cell2').text() !== '')
-            whoWon = 2 - ($('#cell2').text() === 'X');
-
-        if (whoWon) {
-            $('#messagesForPlayers').text(names[whoWon] + ' has won!');
-            $('#informationSpace').append('<button type="button" class="btn btn-info rounded-pill px-3" onclick="replay();">Play Again!</button>');
-            invalidCells();
-        }
     }
 }
 
